fed73ca1f9d093412636bab40ceb70389654360d
Omar Crespo <crespomerchano@gmail.com>
2017-12-13 23:21:59 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <p id="HM">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: "Hola Mundo"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#HM {
+  font-size: 10vw;
+}
+</style>
+
