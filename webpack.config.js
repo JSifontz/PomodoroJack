062778990a564741e2a6ee7f75292784facb78dd
Juan Sifontez <juansfontz@gmail.com>
2018-01-08 04:40:17 -0400

    webpack configurado

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 var path = require('path')
 var webpack = require('webpack')
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -12,53 +16,58 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            }
+          ]
+        })
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react', 'stage-2']
+          }
+        },
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: 'images/[name].[ext]?[hash]'
         }
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css")
+  ],
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      '@': path.resolve(__dirname, './src')
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ['*', '.js', 'jsx', '.json']
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true,
+    noInfo: false,
     overlay: true
   },
   performance: {
-    hints: false
+    hints: "warning"
   },
   devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
